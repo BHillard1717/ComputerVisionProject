e091c3b7b1dfcade5b0c795c607e4156e0760621
BHillard1717 <hillarbp@mail.uc.edu>
2022-02-02 20:47:13 -0500

    Added module for Pose tracking and completed Pose estimation modules - v00.00.03

--- ModuleTestFile.py
+++ ModuleTestFile.py
@@ -1,20 +1,50 @@
 import cv2
 import mediapipe
 import time
+import keyboard
 
 import HandTrackingModule as htm
+import PoseEstimationModule as pem
 
 cap = cv2.VideoCapture(0)
-detector = htm.handDetector()
+detectorHand = htm.handDetector()
+detectorPose = pem.poseDetector()
+
 pTime = 0
 cTime = 0
+detectHand = True
+detectPose = True
+targetPoint = 0
 
 while True:
+    if keyboard.is_pressed('q'):
+        exit(0)
+    elif keyboard.is_pressed(' '):
+        detectHand = not detectHand
+        detectPose = not detectPose
+        print("Switching Mode: Take finger Off Key")
+        time.sleep(1)
+    elif keyboard.is_pressed('p'):
+        detectPose = not detectPose
+        print("Switching Mode: Take finger Off Key")
+        time.sleep(1)
+    elif keyboard.is_pressed('h'):
+        detectHand = not detectHand
+        print("Switching Mode: Take finger Off Key")
+        time.sleep(1)
     success, img = cap.read()
-    img = detector.findHands(img)
-    lmList = detector.findPosition(img)
-    if len(lmList) != 0:
-        print(lmList[12])
+    if detectHand:
+        img = detectorHand.findHands(img)
+        lmList = detectorHand.findPosition(img)
+        if len(lmList) != 0:
+            if lmList[targetPoint]:
+                print(lmList[targetPoint])
+    if detectPose:
+        img = detectorPose.findPose(img)
+        lmList = detectorPose.findPosition(img)
+        if len(lmList) != 0:
+            if lmList[targetPoint]:
+                print(lmList[targetPoint])
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
