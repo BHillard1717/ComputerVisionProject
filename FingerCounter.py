e01427fe62461caa46c0e61a932059e680b44209
BHillard1717 <hillarbp@mail.uc.edu>
2022-02-10 16:19:19 -0500

    Completed base design for finger counter - v00.00.07

--- FingerCounter.py
+++ FingerCounter.py
@@ -1,18 +1,5 @@
-import math
 import cv2
-import mediapipe
 import time
-import keyboard
-import numpy as np
-
-from ctypes import cast, POINTER
-from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(
-    IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = cast(interface, POINTER(IAudioEndpointVolume))
 
 import HandTrackingModule as htm
 cap = cv2.VideoCapture(0)
@@ -20,30 +7,33 @@ detectorHand = htm.handDetector(detectionCon=0.7)
 pTime = 0
 cTime = 0
 targetPoint = 0
-
-volRange = volume.GetVolumeRange()
-minVol = volRange[0]
-maxVol = volRange[2]
-volDist = volRange[2] - volRange[0]
-print(volRange)
-print(minVol, maxVol, volDist)
 while True:
     success, img = cap.read()
     img = detectorHand.findHands(img)
     lmList = detectorHand.findPosition(img, draw=False)
-    #print(lmList)
+    numFingers = 0
+    fingerStr = ""
     if len(lmList) != 0:
         xThumb, yThumb = lmList[4][1], lmList[4][2]
         xIndex, yIndex = lmList[8][1], lmList[8][2]
 
-        cv2.circle(img, (xThumb, yThumb), 15, (255, 0 ,255), cv2.FILLED)
-        cv2.circle(img, (xIndex, yIndex), 15, (255, 0, 255), cv2.FILLED)
-
-        cv2.line(img, (xThumb,yThumb), (xIndex,yIndex), (255,0,255), 5)
-
-
-
-
+        if lmList[8][2] < lmList[6][2]:
+            fingerStr = fingerStr + "Index"
+            numFingers = numFingers + 1
+        if lmList[12][2] < lmList[10][2]:
+            fingerStr = fingerStr + "Middle"
+            numFingers = numFingers + 1
+        if lmList[16][2] < lmList[14][2]:
+            fingerStr = fingerStr + "Ring"
+            numFingers = numFingers + 1
+        if lmList[20][2] < lmList[18][2]:
+            fingerStr = fingerStr + "Pinky"
+            numFingers = numFingers + 1
+        if lmList[4][1] > lmList[8][1] and lmList[4][1] > lmList[3][2]:
+            fingerStr = fingerStr + "Thumb"
+            numFingers = numFingers + 1
+        print(numFingers)
+        print(fingerStr)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
@@ -52,6 +42,7 @@ while True:
     img = cv2.flip(img, 1)
 
     cv2.putText(img, str(fps.__floor__()), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+    cv2.putText(img, f'Number Shown: {numFingers}', (40, 450), cv2.FONT_HERSHEY_PLAIN, 3, (0, 255, 0), 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
--- TestGround.py
+++ TestGround.py
@@ -1,23 +1,27 @@
-import numpy as np
-import cv2
-from mss import mss
-from PIL import Image
-import HandTrackingModule as htm
+# TESTING SCREEN OVERLAY CAPABILITIES
 
-bounding_box = {'top': 0, 'left': 0, 'width': 2000, 'height': 2000}
+#import numpy as np
+#import cv2
+#from mss import mss
+#from PIL import Image
+#import HandTrackingModule as htm
 
-sct = mss()
-detectorHand = htm.handDetector()
-targetPoint = 0
+#bounding_box = {'top': 0, 'left': 0, 'width': 2000, 'height': 2000}
 
-while True:
-    sct_img = sct.grab(bounding_box)
-    sct_img = cv2.cvtColor(np.array(sct_img), cv2.COLOR_RGBA2RGB)
-    cv2.imshow('screen', np.array(sct_img))
-    img = detectorHand.findHands(np.array(sct_img))
-    lmList = detectorHand.findPosition(img)
-    if len(lmList) != 0:
-        if lmList[targetPoint]:
-            print(lmList[targetPoint])
-    cv2.imshow('screen', img)
-    cv2.waitKey(1)
+#sct = mss()
+#detectorHand = htm.handDetector()
+#targetPoint = 0
+
+#while True:
+#    sct_img = sct.grab(bounding_box)
+#    sct_img = cv2.cvtColor(np.array(sct_img), cv2.COLOR_RGBA2RGB)
+#    cv2.imshow('screen', np.array(sct_img))
+#    img = detectorHand.findHands(np.array(sct_img))
+#    lmList = detectorHand.findPosition(img)
+#    if len(lmList) != 0:
+#        if lmList[targetPoint]:
+#            print(lmList[targetPoint])
+#    cv2.imshow('screen', img)
+#    cv2.waitKey(1)
+
+################################################ END TEST ###############################################
